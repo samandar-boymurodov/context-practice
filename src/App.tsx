4f88d63d8cacb12acbdcddc0f3b0393f1ca1fd18
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2022-11-27 21:58:59 +0500

    Move color updating logic to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useMemo, useState } from "react";
-import { CounterContextProvider } from "./Context/CounterContext";
 import { Child1 } from "./Components/Child1";
 import { Child2 } from "./Components/Child2";
 import { Child3 } from "./Components/Child3";
-import { ShowCounter } from "./Components/ShowCounter";
 import { Container } from "./Components/Container";
-import { ColorInput } from "./Components/ColorInput";
 
 // Component is rendered on 3 occasions
 // 1. State change
@@ -13,17 +9,13 @@ import { ColorInput } from "./Components/ColorInput";
 // 3. Parent render
 
 export const App = () => {
-    const [color, setColor] = useState<string>("green");
-
-    const child3 = useMemo(() => <Child3 />, []);
-    const child2 = useMemo(() => <Child2 children={child3} />, [child3]);
-    const child1 = useMemo(() => <Child1 children={child2} />, [child2]);
-
     return (
-        <CounterContextProvider>
-            <ColorInput color={color} setColor={setColor} />
-            <ShowCounter color={color} />
-            <Container>{child1}</Container>
-        </CounterContextProvider>
+        <Container>
+            <Child1>
+                <Child2>
+                    <Child3 />
+                </Child2>
+            </Child1>
+        </Container>
     );
 };
--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -1,15 +1,26 @@
-import { FC, memo, ReactNode, useEffect } from "react";
+import { FC, memo, ReactNode, useEffect, useState } from "react";
+import { CounterContextProvider } from "../Context/CounterContext";
+import { ColorInput } from "./ColorInput";
+import { ShowCounter } from "./ShowCounter";
 
 interface IContainerProps {
     children: ReactNode;
 }
 
 export const Container: FC<IContainerProps> = memo(({ children }) => {
+    const [color, setColor] = useState<string>("green");
+
     useEffect(() => {
         console.log("Container is rendered");
     });
 
-    return <div>{children}</div>;
+    return (
+        <CounterContextProvider>
+            <ColorInput color={color} setColor={setColor} />
+            <ShowCounter color={color} />
+            <div>{children}</div>
+        </CounterContextProvider>
+    );
 });
 
 Container.displayName = "Container";
